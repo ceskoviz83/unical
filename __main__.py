89128eed7992d5183b12250b0dcfc7253de96cad
francesco <lollifr@gmail.com>
2026-02-08 14:45:50 +0100

    aggiunta check connection

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from unical import UnicalConfig,Unical
 import time
 
 if __name__ == "__main__":
-    caldaia = Unical.from_json("config.json")
+    caldaia = Unical.from_json("config/config.json")
     #caldaia.read_polling()
 
     #time.sleep(5)
