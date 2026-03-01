cf1e94e4704f8b9930a3980e5c8477c0ec6c17dc
ceskoviz83 <lollifr@gmail.com>
2026-03-01 19:19:59 +0100

    modificata reference di requirements

--- setup.py
+++ setup.py
@@ -10,17 +10,12 @@ setup(
     description="Unical Modbus TCP - Communication Library",
     install_requires=[
         # Elenca qui le dipendenze, es: 'requests>=2.25.1'
-            "numpy>=2.4.2",
-            "pandas>=2.3.3",
-            "pandas-stubs==2.3.3.260113",
+            "numpy",
+            "pandas",
+            "pandas-stubs",
             "pymodbus>=3.11",
-            "python-dateutil==2.9.0.post0",
-            "pytz==2025.2",
-            "six==1.17.0",
-            "tabulate==0.9.0",
-            "types-pytz==2025.2.0.20251108",
-            "tzdata==2025.3",
-            "python-dotenv==1.0.1",
+            "tabulate>=0.9.0",
+            "python-dotenv",
             "sqlalchemy"
     ],
 )
